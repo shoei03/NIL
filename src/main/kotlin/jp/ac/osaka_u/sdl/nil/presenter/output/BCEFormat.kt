afd587e07818eb4dddddffeb39eec117c45cb25d
shoei03 <shoei.yoshida.03@gmail.com>
2025-10-08 16:38:57 +0900

    feat: update BCEFormat and CSV to include nGramSimilarity and lcsSimilarity in reformat method

--- src/main/kotlin/jp/ac/osaka_u/sdl/nil/presenter/output/BCEFormat.kt
+++ src/main/kotlin/jp/ac/osaka_u/sdl/nil/presenter/output/BCEFormat.kt
@@ -4,11 +4,25 @@ import jp.ac.osaka_u.sdl.nil.entity.CodeBlockInfo
 import java.io.File
 
 /**
- * BigCloneEval Format is as "dir1,file_name1,start_line1,end_line1,dir2,file_name2,start_line2,end_line2"
+ * BigCloneEval Format is as "dir1,file_name1,start_line1,end_line1,dir2,file_name2,start_line2,end_line2,ngram_sim,lcs_sim"
  */
 class BCEFormat : Format() {
-    override fun reformat(codeBlock1: CodeBlockInfo, codeBlock2: CodeBlockInfo): String =
-        "${extractBCEFormat(codeBlock1)},${extractBCEFormat(codeBlock2)}"
+    override fun reformat(
+        codeBlock1: CodeBlockInfo, 
+        codeBlock2: CodeBlockInfo,
+        nGramSimilarity: String?,
+        lcsSimilarity: String?
+    ): String {
+        val similarities = buildString {
+            if (nGramSimilarity != null) {
+                append(",").append(nGramSimilarity)
+            }
+            if (lcsSimilarity != null) {
+                append(",").append(lcsSimilarity)
+            }
+        }
+        return "${extractBCEFormat(codeBlock1)},${extractBCEFormat(codeBlock2)}$similarities"
+    }
 
     private fun extractBCEFormat(codeBlock: CodeBlockInfo): String {
         val (dirName, fileName) = codeBlock.fileName.split(File.separator)
--- src/main/kotlin/jp/ac/osaka_u/sdl/nil/presenter/output/CSV.kt
+++ src/main/kotlin/jp/ac/osaka_u/sdl/nil/presenter/output/CSV.kt
@@ -3,11 +3,25 @@ package jp.ac.osaka_u.sdl.nil.presenter.output
 import jp.ac.osaka_u.sdl.nil.entity.CodeBlockInfo
 
 /**
- * NIL's standard output format is "/path/to/file1,start_line1,end_line1,/path/to/file2,start_line2,end_line2",
+ * NIL's standard output format is "/path/to/file1,start_line1,end_line1,/path/to/file2,start_line2,end_line2,ngram_sim,lcs_sim",
  * as well as CCAligner.
- * Now includes method information if available.
+ * Now includes method information if available and similarity metrics.
  */
 class CSV : Format() {
-    override fun reformat(codeBlock1: CodeBlockInfo, codeBlock2: CodeBlockInfo): String =
-        "${codeBlock1.toFullString()},${codeBlock2.toFullString()}"
+    override fun reformat(
+        codeBlock1: CodeBlockInfo, 
+        codeBlock2: CodeBlockInfo,
+        nGramSimilarity: String?,
+        lcsSimilarity: String?
+    ): String {
+        val similarities = buildString {
+            if (nGramSimilarity != null) {
+                append(",").append(nGramSimilarity)
+            }
+            if (lcsSimilarity != null) {
+                append(",").append(lcsSimilarity)
+            }
+        }
+        return "${codeBlock1.toFullString()},${codeBlock2.toFullString()}$similarities"
+    }
 }
